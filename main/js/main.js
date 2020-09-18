f984dae299325c848ef0da7b332d165e2c9945c7
gdys92 <gdys92@gmail.com>
2020-09-18 17:48:29 -0300

    search box cambios

--- main/js/main.js
+++ main/js/main.js
@@ -1,9 +1,5 @@
 
 
-////crear gifos
-document.getElementById("make-gifs").onclick = function() {
-  window.location.href = "http://file:///C:/Gledys-Muguerza-Gifos/videos/crear-gifos";
-};
 
 
 ////Themes
@@ -21,48 +17,7 @@ them2.addEventListener("click", function(){
   document.querySelector(".logo").src = "./images/gifOF_logo_dark.png";
 })
  
-////Search box////
-const searchForm = document.getElementById('search-form')
-const searchInput = document.getElementById('search-input')
-const resultsEl = document.getElementById('results')
-  searchForm.addEventListener('submit', function(e) {
-    e.preventDefault()
-    const q = searchInput.value
-    search(q)
-  }) 
 
-function search(q) {
-const apikey = '6649Pbqc4KTauggEW8EoUN7pgqbw3g9R'
-const path = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${q}&limit=24&offset=0&rating=G&lang=en`
-
-  fetch(path).then(function(res) {
-     return res.json()
-  }).then(function(json) {
-    console.log(json.data[0].images.original.url)
-    
-    let resultsHTML = ''
-   
-    json.data.forEach(function(obj) {
-      console.log(obj)
-
-      const url = obj.images.original.url
-      const width = obj.images.original.width
-      const height = obj.images.original
-      const title = obj.title
-      resultsHTML += `<img 
-      class="item"
-      src="${url}" 
-      widht="${width}" 
-      height="${height}"
-      alt="${title}">`
-    })
-   
-    resultsEl.innerHTML = resultsHTML
-  }).catch(function(err) {
-    console.log(err.message)
-  })
-
-  }
 
   ////Tendencias/////
 const trendingEl = document.getElementById('trending')
